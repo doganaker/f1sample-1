cb307470cf5ae95e067f28bd1fb08d9ab95f3df7
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2020-10-20 15:23:00 +0300

    asenkron

--- f1.js
+++ f1.js
@@ -1,7 +1,7 @@
 const url = 'https://ergast.com/api/f1/'
 const f1manager = {
 
-    getraces: (year, round) => {
+     getraces:  (year, round) => {
         fetch(url + year + '/' + round + '/results.json')
             .then((res) => res.json())
             .then((data) => {
@@ -14,14 +14,49 @@ const f1manager = {
 
 
 
-function getraces2(year, round) {
-    fetch(url + year + '/' + round + '/results.json')
-        .then((res) => res.json())
-        .then((data) => {
-            return data;
-        })
+async function getraces2(year, round) {
+
+  var result =  await fetch(url + year + '/' + round + '/results.json')
+        .then((res) => res.json());
+    
+        return result;
+
+
 }
 
 
-let x = getraces2(2008,1);
+getraces2(2008, 1).then((data) => {
+     console.log(data);
+})
+
+//let x = await getraces2(2008,1);
 // console.log(x);
+
+
+
+async function Merhaba() {
+
+    return "Çağatay";
+}
+
+Merhaba()
+    .then((msg) => {
+        //console.log(msg);
+    })
+
+
+
+async function Ornek() {
+    await fetch(url + 2008 + '/' + 1 + '/results.json')
+        .then((res) => res.json())
+        .then((data) => {
+          //  console.log(data);
+        })
+
+   // console.log('Şoko')
+}
+
+
+//console.log('BERK');
+
+
